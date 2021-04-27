546680539dba1dc2ada218a3a5fff56eec8a267e
Gregory Bowles <sbowles21@basecampcodingacademy.org>
2021-04-27 11:56:19 -0500

    yes

--- app.js
+++ app.js
@@ -16,3 +16,44 @@ function change() {
     document.getElementById("themename").textContent = " Blue Theme";
   }
 }
+
+function hideImages(){
+  if (document.getElementById("img1").classList.contains("shown")){
+    document.getElementById("img1").classList.remove("shown")
+    document.getElementById("img1").classList.add("hidden")
+    document.getElementById("Python-box1").style.height = "225px"
+    document.getElementById("what_do").textContent = "Show"
+  } else{
+    document.getElementById("img1").classList.remove("hidden")
+    document.getElementById("img1").classList.add("shown")
+    document.getElementById("what_do").textContent = "Hide"
+    document.getElementById("Python-box1").style.height = "auto"
+  }
+  if (document.getElementById("img2").classList.contains("shown")){
+    document.getElementById("img2").classList.remove("shown")
+    document.getElementById("img2").classList.add("hidden")
+    document.getElementById("Python-box2").style.height = "200px"
+  } else{
+    document.getElementById("img2").classList.remove("hidden")
+    document.getElementById("img2").classList.add("shown")
+    document.getElementById("Python-box2").style.height = "628px"
+  }
+  if (document.getElementById("img3").classList.contains("shown")){
+    document.getElementById("img3").classList.remove("shown")
+    document.getElementById("img3").classList.add("hidden")
+    document.getElementById("Html_box1").style.height = "190px"
+  } else{
+    document.getElementById("img3").classList.remove("hidden")
+    document.getElementById("img3").classList.add("shown")
+    document.getElementById("Html_box1").style.height = "560px"
+  }
+  if (document.getElementById("img4").classList.contains("shown")){
+    document.getElementById("img4").classList.remove("shown")
+    document.getElementById("img4").classList.add("hidden")
+    document.getElementById("Html_box2").style.height = "225px"
+  } else{
+    document.getElementById("img4").classList.remove("hidden")
+    document.getElementById("img4").classList.add("shown")
+    document.getElementById("Html_box2").style.height = "560px"
+  }
+}
