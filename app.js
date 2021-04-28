e408e9142bd8c8855a98e3c07ecd7671fbd39a9b
Gregory Bowles <sbowles21@basecampcodingacademy.org>
2021-04-28 11:51:08 -0500

    changing alot

--- app.js
+++ app.js
@@ -1,59 +1,59 @@
+let theme = document.getElementById("theme")
+let changebutton = document.getElementById("changebutton")
+let pythoncolor = document.getElementById("pythona")
+let htmlcolor = document.getElementById("htmla")
+let djangocolor = document.getElementById("djangoa")
+let jscolor = document.getElementById("jsa")
+let contactStuff = document.getElementsByClassName("contactStuff")
+
 function change() {
-  if (document.getElementById("theme").classList.contains("purpletheme")) {
-    document.getElementById("theme").classList.remove("purpletheme");
-    document.getElementById("theme").classList.add("bluetheme");
-  } else if (document.getElementById("theme").classList.contains("bluetheme")) {
-    document.getElementById("theme").classList.remove("bluetheme");
-    document.getElementById("theme").classList.add("purpletheme");
+  if (theme.classList.contains("purpletheme")) {
+    theme.classList.remove("purpletheme");
+    theme.classList.add("bluetheme");
+  } else if (theme.classList.contains("bluetheme")) {
+    theme.classList.remove("bluetheme");
+    theme.classList.add("purpletheme");
   }
-  if (document.getElementById("changebutton").classList.contains("purplebutton")) {
-    document.getElementById("changebutton").classList.remove("purplebutton");
-    document.getElementById("changebutton").classList.add("bluebutton");
+  if (changebutton.classList.contains("purplebutton")) {
+    changebutton.classList.remove("purplebutton");
+    changebutton.classList.add("bluebutton");
     document.getElementById("themename").textContent = " Purple Theme";
-  } else if (document.getElementById("changebutton").classList.contains("bluebutton")) {
-    document.getElementById("changebutton").classList.remove("bluebutton");
-    document.getElementById("changebutton").classList.add("purplebutton");
+  } else if (changebutton.classList.contains("bluebutton")) {
+    changebutton.classList.remove("bluebutton");
+    changebutton.classList.add("purplebutton");
     document.getElementById("themename").textContent = " Blue Theme";
   }
-}
-
-function hideImages(){
-  if (document.getElementById("img1").classList.contains("shown")){
-    document.getElementById("img1").classList.remove("shown")
-    document.getElementById("img1").classList.add("hidden")
-    document.getElementById("Python-box1").style.height = "225px"
-    document.getElementById("what_do").textContent = "Show"
-  } else{
-    document.getElementById("img1").classList.remove("hidden")
-    document.getElementById("img1").classList.add("shown")
-    document.getElementById("what_do").textContent = "Hide"
-    document.getElementById("Python-box1").style.height = "auto"
+  /*To Change Text color*/
+  /* Python */
+  if(pythoncolor.classList.contains("blueText")){
+    pythoncolor.classList.remove("blueText");
+    pythoncolor.classList.add("greyText");
+  } else if(pythoncolor.classList.contains("greyText")){
+    pythoncolor.classList.remove("greyText");
+    pythoncolor.classList.add("blueText");
   }
-  if (document.getElementById("img2").classList.contains("shown")){
-    document.getElementById("img2").classList.remove("shown")
-    document.getElementById("img2").classList.add("hidden")
-    document.getElementById("Python-box2").style.height = "200px"
-  } else{
-    document.getElementById("img2").classList.remove("hidden")
-    document.getElementById("img2").classList.add("shown")
-    document.getElementById("Python-box2").style.height = "628px"
+  /* Html */
+  if(htmlcolor.classList.contains("blueText")){
+    htmlcolor.classList.remove("blueText");
+    htmlcolor.classList.add("greyText");
+  } else if(htmlcolor.classList.contains("greyText")){
+    htmlcolor.classList.remove("greyText");
+    htmlcolor.classList.add("blueText");
   }
-  if (document.getElementById("img3").classList.contains("shown")){
-    document.getElementById("img3").classList.remove("shown")
-    document.getElementById("img3").classList.add("hidden")
-    document.getElementById("Html_box1").style.height = "190px"
-  } else{
-    document.getElementById("img3").classList.remove("hidden")
-    document.getElementById("img3").classList.add("shown")
-    document.getElementById("Html_box1").style.height = "560px"
+  /* Django */
+  if(djangocolor.classList.contains("blueText")){
+    djangocolor.classList.remove("blueText");
+    djangocolor.classList.add("greyText");
+  } else if(djangocolor.classList.contains("greyText")){
+    djangocolor.classList.remove("greyText");
+    djangocolor.classList.add("blueText");
   }
-  if (document.getElementById("img4").classList.contains("shown")){
-    document.getElementById("img4").classList.remove("shown")
-    document.getElementById("img4").classList.add("hidden")
-    document.getElementById("Html_box2").style.height = "225px"
-  } else{
-    document.getElementById("img4").classList.remove("hidden")
-    document.getElementById("img4").classList.add("shown")
-    document.getElementById("Html_box2").style.height = "560px"
+  /* Javascript */
+  if(jscolor.classList.contains("blueText")){
+    jscolor.classList.remove("blueText");
+    jscolor.classList.add("greyText");
+  } else if(jscolor.classList.contains("greyText")){
+    jscolor.classList.remove("greyText");
+    jscolor.classList.add("blueText");
   }
 }
